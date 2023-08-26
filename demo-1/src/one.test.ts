53b6bfd1c59c593317dcbe0eb6f6f2f0a7988346
Rudi Starcevic <rudi.starcevic@pm.me>
2023-08-27 00:20:17 +0800

    Updates

--- demo-1/src/one.test.ts
+++ demo-1/src/one.test.ts
@@ -17,12 +17,31 @@ describe("makeGetRequest", () => {
     const url = "https://dummyjson.com/products/1"
 
     const expectedResponse = {
+      // `data` is the response that was provided by the server
       data: {
         key: "value",
       },
+
+      // `status` is the HTTP status code from the server response
       status: 200,
+
+      // `statusText` is the HTTP status message from the server response
+      // As of HTTP/2 status text is blank or unsupported.
+      // (HTTP/2 RFC: https://www.rfc-editor.org/rfc/rfc7540#section-8.1.2.4)
       statusText: "OK",
+
+      // `headers` the HTTP headers that the server responded with
+      // All header names are lower cased and can be accessed using the bracket notation.
+      // Example: `response.headers['content-type']`
       headers: {},
+
+      // `config` is the config that was provided to `axios` for the request
+      config: {},
+
+      // `request` is the request that generated this response
+      // It is the last ClientRequest instance in node.js (in redirects)
+      // and an XMLHttpRequest instance in the browser
+      request: {},
     }
 
     mockJest = jest.spyOn(axios, "get").mockResolvedValueOnce(expectedResponse)
@@ -32,5 +51,23 @@ describe("makeGetRequest", () => {
 
     expect(result).toEqual(expectedResponse)
     expect(mockJest).toHaveBeenCalledWith(url)
+  })
+
+  it("should make a GET request to the specified URL", async () => {
+    const url = "https://dummyjson.com/products/1"
+
+    const expectedResponse = {
+      data: {
+        key: "value",
+      },
+      status: 200,
+      statusText: "OK",
+      headers: {},
+    }
+
+    mockJest = jest.spyOn(axios, "get").mockRejectedValueOnce(Error("Boom!"))
+
+    const result = await makeGetRequest(axios, url)()
+    console.log(JSON.stringify(result, null, 2))
   })
 })
--- demo-1/src/one.ts
+++ demo-1/src/one.ts
@@ -1,11 +1,32 @@
-import { AxiosStatic } from "axios"
+import { AxiosStatic, AxiosError } from "axios"
 
 export function makeGetRequest(axiosInstance: AxiosStatic, url: string) {
   return async () => {
     try {
       return await axiosInstance.get(url)
     } catch (error) {
-      return `Error making GET request: ${error}`
+      if (axiosInstance.isAxiosError(error)) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data)
+          console.log(error.response.status)
+          console.log(error.response.headers)
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request)
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message)
+        }
+        // Do something with this error...
+        console.log(error.toJSON())
+      } else {
+        // console.error(error)
+        return `Error 201 making GET request: ${error}`
+      }
     }
   }
 }
